45ae2f92e179149cbb3c876ec45a16591747297f
ducvt <duccc0926@gmail.com>
2022-05-31 14:49:19 +0700

    fix route when create success tournament

--- components/ui/tournament/create/notify/notifyModal.tsx
+++ components/ui/tournament/create/notify/notifyModal.tsx
@@ -19,7 +19,7 @@ export default observer(function NotifyModal(props: Props) {
     setIsModalVisible(false);
     TournamentStore.resetStates();
     clearLocalCreateTournament();
-    router.push("/");
+    router.push("/tournament");
     TournamentStore.depositModalVisible = false;
   };
 
@@ -27,7 +27,7 @@ export default observer(function NotifyModal(props: Props) {
     setIsModalVisible(false);
     TournamentStore.resetStates();
     clearLocalCreateTournament();
-    router.push("/");
+    router.push("/tournament");
     TournamentStore.depositModalVisible = false;
   };
 
