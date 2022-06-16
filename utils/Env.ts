52155cef7ecd7ba5f5396eb561580f61ca66f79e
luatnd <neo.mxn0@gmail.com>
2022-06-16 13:39:52 +0700

    Inject app_env into app Add app_env into page title for better GA experience

--- utils/Env.ts
+++ utils/Env.ts
@@ -1,4 +1,6 @@
 export const isClient = typeof window !== "undefined";
 export const isDevMode = process.env.NODE_ENV === "development";
 export const isClientDevMode = isDevMode && isClient;
-console.log('{} isClient, isDevMode, isClientDevMode: ', isClient, isDevMode, isClientDevMode);
+export const app_env: string = "APP_ENV"; // This will be override in compile phase by webpack
+
+console.log('{} app_env, isClient, isDevMode, isClientDevMode: ', app_env, isClient, isDevMode, isClientDevMode);
