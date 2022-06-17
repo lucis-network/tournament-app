5eb5b84c048f8028bb4fbc57f094d66140ea5465
VU LONG VU <longvuit18@gmail.com>
2022-06-17 14:30:22 +0700

    add router p2e 20%

---
+++ pages/p2e/dashboard/index.tsx
@@ -0,0 +1,16 @@
+import Dashboard from "components/ui/p2e/dashboard";
+import P2EWrapper from "components/ui/p2e/p2eWrapper";
+import React from "react"
+
+
+const DashboardPage = () => {
+    return (
+        <>
+            <P2EWrapper>
+                <Dashboard />
+            </P2EWrapper>
+        </>
+    );
+}
+
+export default DashboardPage;
---
+++ pages/p2e/overview1/index.tsx
@@ -0,0 +1,175 @@
+import React, { useEffect, useState } from 'react'
+import s from './P2EOverview.module.sass'
+import { Button, Col, Image, Row } from "antd";
+import { isEmpty } from "lodash";
+import { isDevMode } from "../../../utils/Env";
+import { PlatformAccount } from "../../../src/generated/graphql_p2e";
+import { observer } from 'mobx-react-lite';
+import { useGetPlatformAccount } from 'hooks/p2e/useP2E';
+import LoginBoxStore from 'components/Auth/Login/LoginBoxStore';
+import AuthStore from 'components/Auth/AuthStore';
+import P2EWrapper from 'components/ui/p2e/p2eWrapper';
+type P2EOverviewProps = {
+  faceitUser: PlatformAccount,
+  isLoadingFaceitUser: boolean
+}
+export default observer(function P2EOverview(props: P2EOverviewProps) {
+  const [faceitLogin, setFaceitLogin] = useState({
+    login: () => { }
+  })
+  const [faceitUser, setFaceitUser] = useState<PlatformAccount>(props.faceitUser);
+  const [isAuth, setIsAuth] = React.useState<boolean>(false);
+  const { refetchPlatformAccount } = useGetPlatformAccount()
+  const fetchJsFromCDN = (src: string, externals: any[] = []) => {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.setAttribute("src", src);
+      script.addEventListener("load", () => {
+        resolve(
+          externals.map((key) => {
+            const ext = window[key];
+            typeof ext === "undefined" &&
+              console.warn(`No external named '${key}' in window`);
+            return ext;
+          })
+        );
+      });
+      script.addEventListener("error", reject);
+      document.body.appendChild(script);
+    });
+  };
+
+  useEffect(() => {
+    if (isEmpty(faceitUser)) {
+      fetchJsFromCDN(
+        "https://cdn.faceit.com/oauth/faceit-oauth-sdk-1.2.7.min.js",
+        ["FACEIT"]
+      ).then(([FACEIT]: any) => {
+        function callback(response: any) {
+          if (response.isIdTokenValid === true) {
+            return;
+          }
+          alert("The id token is not valid, something went wrong");
+        }
+        const initParams = {
+          client_id: isDevMode
+            ? process.env.NEXT_PUBLIC_FACEIT_CLIENT_ID_DEV
+            : process.env.NEXT_PUBLIC_FACEIT_CLIENT_ID_TEST,
+          response_type: "token",
+        };
+        FACEIT.init(initParams, callback);
+        setFaceitLogin({
+          login: FACEIT.loginWithFaceit,
+        });
+      });
+    }
+  }, [faceitUser])
+
+  useEffect(() => {
+    handleRefetchPlatformAccount();
+  }, [AuthStore.isLoggedIn])
+
+  const handleRefetchPlatformAccount = async () => {
+    try {
+      const a = await refetchPlatformAccount();
+      setFaceitUser(a.data?.getPlatformAccount[0]);
+      setIsAuth(true);
+    } catch (error) {
+      // TODO
+      setFaceitUser({} as any);
+      setIsAuth(false);
+    }
+
+  }
+
+  const handleConnectFaceit = () => {
+    if (!isAuth) {
+      LoginBoxStore.connectModalVisible = true;
+      return;
+    }
+
+    faceitLogin.login();
+  }
+
+
+  // const loginWithSteam = () => {
+  //   steam.resolve("https://steamcommunity.com/id/IMAPOORKID").then((id: string) => {
+  //     console.log(id); // 76561198146931523
+  //   });
+  // };
+
+  return (
+    <>
+      <P2EWrapper>
+        <div className="lucis-container-2">
+          <div className={s.overviewContainer}>
+            <div className={s.overviewSection}>
+              <h2 className={s.overviewSectionTitle}>Choose game</h2>
+              <Row>
+                <Col xs={{ span: 24 }} sm={{ span: 12 }}>
+                  <div className={s.gameItem}>
+                    <div className={s.gameImage}>
+                      <Image
+                        src="/assets/P2E/lol-cover.jpg"
+                        preview={false}
+                        alt=""
+                        className={s.gameCover}
+                      />
+                      <Image
+                        src="/assets/P2E/lol-logo.webp"
+                        preview={false}
+                        alt=""
+                        className={s.gameLogo}
+                      />
+                    </div>
+                    <div className={s.gameName}>
+                      <h3>League of Legends</h3>
+                      <Button className={s.btnConnectLol} disabled>Connect game</Button>
+                    </div>
+                  </div>
+                </Col>
+                <Col xs={{ span: 24 }} sm={{ span: 12 }}>
+                  <div className={s.gameItem}>
+                    <div className={s.gameImage}>
+                      <Image
+                        src="/assets/P2E/csgo-cover.webp"
+                        preview={false}
+                        alt=""
+                        className={s.gameCover}
+                      />
+                      <Image
+                        src="/assets/P2E/csgo-logo.webp"
+                        preview={false}
+                        alt=""
+                        className={s.gameLogo}
+                      />
+                    </div>
+                    <div className={s.gameName}>
+                      <h3>CS:GO FACEIT</h3>
+                      {isEmpty(faceitUser) ? (
+                        <>
+                          {/*<div id="faceitLogin" className={s.btnConnectGame}></div>*/}
+                          <Button
+                            onClick={() => handleConnectFaceit()}
+                            className={s.btnLoginFaceit}
+                            loading={props.isLoadingFaceitUser}
+                          >CONNECT WITH FACEIT</Button>
+                        </>
+                      ) : (
+                        <div className={s.platformUser}>
+                          <Image src={faceitUser?.avatar ? faceitUser?.avatar : "/assets/avatar.jpg"} preview={false} alt="" className={s.platformUserAvatar} />
+                          <div className={s.platformUserName}>{faceitUser?.nick_name}</div>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </div>
+        </div>
+      </P2EWrapper>
+    </>
+  )
+}
+)
