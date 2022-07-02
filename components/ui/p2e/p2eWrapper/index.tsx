335a6594f60baf7d4c8ec53714ce47bf9cef3c31
chaupa <chaupa.fe@gmail.com>
2022-07-02 13:59:17 +0700

    WIP: raffles UI

--- components/ui/p2e/p2eWrapper/index.tsx
+++ components/ui/p2e/p2eWrapper/index.tsx
@@ -7,7 +7,8 @@ import AuthStore from "components/Auth/AuthStore";
 import { observer } from "mobx-react-lite";
 
 interface IProps {
-  children: React.ReactChild | React.ReactChild[]
+  children: React.ReactChild | React.ReactChild[],
+  mainClassname?: string,
 }
 export default observer(function P2EWrapper(props: IProps) {
   const router = useRouter();
@@ -41,17 +42,13 @@ export default observer(function P2EWrapper(props: IProps) {
   ];
 
   const isDisabledTab = (tab: string) => {
-    if (tab === "/p2e/raffles" || tab === "/p2e/items" || tab === "/p2e/battle-pass") {
-      return true;
-    }
-
-    return false;
+    return tab === "/p2e/items" || tab === "/p2e/battle-pass";
   }
 
   return (
     <>
       <DocHead />
-      <main style={{ minHeight: "100vh" }} className={s.homeWrap}>
+      <main style={{ minHeight: "100vh" }} className={`${s.homeWrap} ${props.mainClassname ?? ''}`}>
         <div className={`${s.p2eWrap}`}>
           <div className={s.tabsWrap}>
             <div className="lucis-container-2">
