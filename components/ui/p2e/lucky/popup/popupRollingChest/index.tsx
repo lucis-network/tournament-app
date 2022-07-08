cc8ac20fda239f2144cc6c69a9460674231d76db
The447 <dthe0412@gmail.com>
2022-07-08 13:48:05 +0700

    update autorolling

--- components/ui/p2e/lucky/popup/popupRollingChest/index.tsx
+++ components/ui/p2e/lucky/popup/popupRollingChest/index.tsx
@@ -1,6 +1,6 @@
 import { Modal } from "antd";
 import { useWindowSize } from "hooks/useWindowSize";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import s from "./index.module.sass";
 
 type Props = {
@@ -11,7 +11,44 @@ type Props = {
 const PopupRollingChest = (props: Props) => {
   const { status, closePopupRollingChest } = props;
   const [width] = useWindowSize();
-  const [autoRolling, setAutoScroll] = useState(false)
+  const [autoRolling, setAutoRolling] = useState(false)
+
+  const styles = {
+    // width: width < 768
+    // ? width - 50 
+    // : width < 1280
+    // ? width - 100 
+    // : width - 220 ,
+    
+    transform: autoRolling ? 'translateX(-6000px)': 'translateX(0px)'
+  }
+
+  const datas = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+  ]
+
+  useEffect(() => {
+    if (status == true) {
+      setAutoRolling(false)
+    }
+  }, [status])
 
   return (
     <div className={s.wrapper_popup_rolling_chest}>
@@ -23,118 +60,65 @@ const PopupRollingChest = (props: Props) => {
         onCancel={closePopupRollingChest}
         width={"100%"}
       >
+            <button onClick={() => setAutoRolling(true)}>run</button>
         <div className={s.container}>
           <div className={s.bg_rolling}>
             <img src="/assets/P2E/luckychest/bg_rolling.png" alt="" />
           </div>
+          <div style={{maxWidth:' 1440px', overflow: 'hidden', margin: '0 auto'}}>
           <div
             className={s.rolling}
-            style={
-              width < 768
-                ? { width: width - 50 }
-                : width < 1280
-                ? { width: width - 100 }
-                : { width: width - 220 }
-            }
+            style={styles}
           >
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
-
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
-
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
-            <div className={s.box_item}>
-              <div>
-                <img src="/assets/P2E/luckychest/im_box.png" alt="" />
-              </div>
-              <div className={s.title}>
-                <p>Good luck!</p>
-              </div>
-            </div>
+              {
+                datas.map((e: any, i: any) => {
+                  return (
+                    <>
+                      <div className={s.box_item}>
+                        <div>
+                          <img src="/assets/P2E/luckychest/im_box.png" alt="" />
+                        </div>
+                        <div className={s.title}>
+                          <p>Good luck!</p>
+                        </div>
+                      </div>
+                      <div className={s.box_item}>
+                        <div>
+                          <img src="/assets/P2E/luckychest/im_box.png" alt="" />
+                        </div>
+                        <div className={s.title}>
+                          <p>Good luck!</p>
+                        </div>
+                      </div>
+                      <div className={s.box_item}>
+                        <div>
+                          <img src="/assets/P2E/luckychest/im_box.png" alt="" />
+                        </div>
+                        <div className={s.title}>
+                          <p>Good luck!</p>
+                        </div>
+                      </div>
+                      <div className={s.box_item}>
+                        <div>
+                          <img src="/assets/P2E/luckychest/im_box.png" alt="" />
+                        </div>
+                        <div className={s.title}>
+                          <p>Good luck!</p>
+                        </div>
+                      </div>
+                      <div className={s.box_item}>
+                        <div>
+                          <img src="/assets/P2E/luckychest/im_box.png" alt="" />
+                        </div>
+                        <div className={s.title}>
+                          <p>Good luck!</p>
+                        </div>
+                      </div>
+                    </>
+                  );
+                })
+              }
+          </div>
           </div>
         </div>
       </Modal>
