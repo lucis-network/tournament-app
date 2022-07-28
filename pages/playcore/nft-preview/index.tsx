f9bba3f94199ccdd86aafa92073399a1230bc03c
luatnd <neo.mxn0@gmail.com>
2022-07-28 16:01:35 +0700

    change nft mixer uri

--- pages/playcore/nft-preview/index.tsx
+++ pages/playcore/nft-preview/index.tsx
@@ -59,7 +59,7 @@ const NftPreviewPage = () => {
       glass: glasses,
       weapon: weapon,
     })
-    const genNftUrl = 'http://149.28.157.158:8080/v1/image/mixin?' + queryString;
+    const genNftUrl = 'https://nft-img-mixer.lucis.network/v1/image/mixin?' + queryString;
     console.log('{genNft} genNftUrl: ', genNftUrl);
 
     postData(genNftUrl)
