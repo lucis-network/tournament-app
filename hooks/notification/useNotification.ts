25ec08dee80f8376ef70e877af802d4325e71986
chaupa <chaupa.fe@gmail.com>
2022-08-23 15:56:10 +0700

    WIP: notification

---
+++ hooks/notification/useNotification.ts
@@ -0,0 +1,69 @@
+import { gql, useMutation, useQuery, useSubscription } from "@apollo/client";
+import { notification } from "antd";
+import {isEmpty} from "lodash";
+
+type UseSubscribtionNotificationProps = {
+  user_id: number | undefined;
+};
+
+export const useGetNotification = () => {
+  const {
+    loading: getNotificationLoading,
+    error: getNotificationError,
+    data,
+  } = useQuery(
+    GET_NOTIFICATION,
+    {
+      fetchPolicy: "no-cache"
+    }
+  );
+
+  return {
+    getNotificationLoading,
+    getNotificationError,
+    getNotificationData: data?.getNotification,
+  };
+};
+
+export const useSubscribtionNotification = ({ user_id }: UseSubscribtionNotificationProps) => {
+  const {
+    loading,
+    data: notificationSubscriptionData
+  } = useSubscription(PUSH_NOTIFICATION, {
+    variables: {
+      user_id: parseFloat(user_id + '')
+    },
+    fetchPolicy: "no-cache",
+    // skip: isEmpty(user_id)
+  });
+
+  return {
+    notificationSubscriptionData: notificationSubscriptionData?.pushNotification
+  }
+}
+
+const PUSH_NOTIFICATION = gql`
+  subscription ($user_id: Float!) {
+    pushNotification(user_id: $user_id) {
+      user_id
+      title
+      image
+      is_seen
+      content
+    }
+  }
+`;
+
+const GET_NOTIFICATION = gql`
+  query {
+    getNotification {
+      id
+      user_id
+      is_seen
+      title
+      image
+      content
+      created_at
+    }
+  }
+`;
