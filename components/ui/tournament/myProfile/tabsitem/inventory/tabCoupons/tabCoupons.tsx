356037e6f9837f396c51e21c559153c170c04f31
loinv22 <loinv226@gmail.com>
2022-09-27 11:35:51 +0700

    Update: add inventory filter

--- components/ui/tournament/myProfile/tabsitem/inventory/tabCoupons/tabCoupons.tsx
+++ components/ui/tournament/myProfile/tabsitem/inventory/tabCoupons/tabCoupons.tsx
@@ -5,6 +5,7 @@ import debounce from "lodash/debounce";
 import CouponItem from "./couponItem";
 import AuthStore, { AuthUser } from "../../../../../../Auth/AuthStore";
 import { useMyCoupon } from "hooks/myProfile/useCoupon";
+import { CouponType } from "src/generated/graphql";
 
 type Props = {
   isOwner?: boolean;
@@ -16,11 +17,13 @@ const { Option } = Select;
 const CouponTabInventory = (props: Props) => {
   const { isOwner, userInfo } = props;
   const [searchName, setSearchName] = useState<string>("");
+  const [couponType, setCouponType] = useState<string>();
 
   const { dataMyInventoryCoupons, loading, refetchMyInventoryCoupon } =
     useMyCoupon({
       user_id: isOwner ? AuthStore.id || undefined : Number(userInfo.id),
       search_name: searchName,
+      type: couponType,
     });
 
   const onSearch = (e: any) => {
@@ -34,17 +37,47 @@ const CouponTabInventory = (props: Props) => {
     []
   );
 
+  const handleChange = (value: string) => {
+    if (value === "") {
+      setCouponType(undefined);
+      return;
+    }
+    setCouponType(value);
+  };
+
   return (
     <>
       <div className={s.groupSearch}>
         <div>
-          {/*<img src="/assets/home/ic_member.svg" alt="" />*/}
+          <Select
+            defaultValue="All"
+            className={s.dropdownSearch}
+            onChange={handleChange}
+          >
+            <Option value="">All</Option>
+            <Option value={CouponType.Raffle}>
+              {CouponType.Raffle.toUpperCase()}
+            </Option>
+            <Option value={CouponType.BuyBox}>
+              {CouponType.BuyBox.toUpperCase()}
+            </Option>
+          </Select>
+        </div>
+        <div>
           <Input
             placeholder="Search"
             onChange={onSearch}
             className={`${s.searchText}`}
           />
         </div>
+
+        {/* <div>
+          <Input
+            placeholder="Search"
+            onChange={onSearch}
+            className={`${s.searchText}`}
+          />
+        </div> */}
       </div>
       <div className={s.listItemsInventory}>
         {dataMyInventoryCoupons &&
